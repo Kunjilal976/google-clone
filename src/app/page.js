cb80a36bc70f14549b0f1e1195c7059673cc11ff
Kunji Lal Meena <kunjilal976@gmail.com>
2024-06-14 17:35:41 +0530

    created heade page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import HomeHeader from '@/components/HomeHeader'
 
-export default function page() {
+export default function Home() {
   return (
-    <div>page</div>
+    <>
+    <HomeHeader/>
+    </>
   )
 }
